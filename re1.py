f10e7774de9f6b19bda122bdc9d09062b9c7c0e7
Skant <1336614368@qq.com>
2019-07-18 10:57:24 +0800

    init add

---
+++ re1.py
@@ -0,0 +1,7 @@
+# import re
+#
+# re.search(r'(ab)+','ababababababababa').group()
+#
+# print(re.search(r'(王|李)\w{1,3}','王者荣耀').group())
+#
+# print(re.search(r'([0-9][1-9]{1,16}[0-9A-Z])','41080319941202005X').group())
